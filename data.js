dede7ced580d4efd354bf13979044f53b1e316d7
AntonSmeljov <antonsmeljov@gmail.com>
2025-01-21 13:43:27 +0100

    Commit

---
+++ data.js
@@ -0,0 +1,651 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hodnik-1",
+      "name": "Hodnik 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.3436711469767646,
+        "pitch": 0.11151042025775126,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5339132273467184,
+          "pitch": 0.19896237853543575,
+          "rotation": 0,
+          "target": "5-dnevni-borovak"
+        },
+        {
+          "yaw": 2.329751269544012,
+          "pitch": 0.08799196155470668,
+          "rotation": 0,
+          "target": "6-kuhinja"
+        },
+        {
+          "yaw": 1.8821490267507777,
+          "pitch": 0.022239170837218936,
+          "rotation": 0,
+          "target": "13-terasa"
+        },
+        {
+          "yaw": -3.07195253574384,
+          "pitch": 0.24813569288660453,
+          "rotation": 0,
+          "target": "1-hodnik-2"
+        },
+        {
+          "yaw": -0.2231868132070698,
+          "pitch": 0.1406083240377516,
+          "rotation": 0,
+          "target": "8-hodnik-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hodnik-2",
+      "name": "Hodnik 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.40481657211052813,
+        "pitch": 0.032268081992222974,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.012454340834279876,
+          "pitch": 0.2657751938743331,
+          "rotation": 0,
+          "target": "0-hodnik-1"
+        },
+        {
+          "yaw": 1.6138966976046705,
+          "pitch": 0.12236724834044921,
+          "rotation": 0,
+          "target": "4-kupatilo"
+        },
+        {
+          "yaw": 2.379634657242063,
+          "pitch": 0.11737097170358268,
+          "rotation": 0,
+          "target": "2-spavaa-soba-1"
+        },
+        {
+          "yaw": 3.129051210408547,
+          "pitch": 0.08096560836966304,
+          "rotation": 0,
+          "target": "3-ostava"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-spavaa-soba-1",
+      "name": "Spavaća soba 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.5973717693845835,
+        "pitch": 0.18686220826963584,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.499221339812756,
+          "pitch": 0.07393872459551432,
+          "rotation": 0,
+          "target": "1-hodnik-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ostava",
+      "name": "Ostava",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.2724758447830995,
+        "pitch": 0.11746009999878737,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1124079167610574,
+          "pitch": 0.10187365581440311,
+          "rotation": 0,
+          "target": "1-hodnik-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kupatilo",
+      "name": "Kupatilo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.4862669277077103,
+        "pitch": 0.1822725956923552,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7406208878058944,
+          "pitch": 0.07426607294151921,
+          "rotation": 0,
+          "target": "1-hodnik-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dnevni-borovak",
+      "name": "Dnevni borovak",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.6679535948969093,
+        "pitch": 0.1072475035596323,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6566752569776106,
+          "pitch": 0.11686658490674162,
+          "rotation": 0,
+          "target": "13-terasa"
+        },
+        {
+          "yaw": 1.557942030888773,
+          "pitch": 0.12811824228766966,
+          "rotation": 0,
+          "target": "6-kuhinja"
+        },
+        {
+          "yaw": -2.9855490705486893,
+          "pitch": 0.16988811869889275,
+          "rotation": 0,
+          "target": "0-hodnik-1"
+        },
+        {
+          "yaw": -2.459464749459933,
+          "pitch": 0.009080900867912689,
+          "rotation": 0,
+          "target": "1-hodnik-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kuhinja",
+      "name": "Kuhinja",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.938216818571691,
+        "pitch": 0.04693476604039937,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9041498210703907,
+          "pitch": 0.1476826180429356,
+          "rotation": 0,
+          "target": "5-dnevni-borovak"
+        },
+        {
+          "yaw": -2.5024439845115527,
+          "pitch": 0.13077808940140478,
+          "rotation": 0,
+          "target": "0-hodnik-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-spavaa-soba-2",
+      "name": "Spavaća soba 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.8501685303625806,
+        "pitch": 0.096620133187308,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0719591555504664,
+          "pitch": 0.09415397465182984,
+          "rotation": 0,
+          "target": "8-hodnik-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-hodnik-3",
+      "name": "Hodnik 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.0794277855259438,
+        "pitch": 0.048306467405977216,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9739757762754788,
+          "pitch": 0.12734241754320585,
+          "rotation": 0,
+          "target": "7-spavaa-soba-2"
+        },
+        {
+          "yaw": 2.556237824552319,
+          "pitch": 0.09887898095771597,
+          "rotation": 0,
+          "target": "12-hodnik-4"
+        },
+        {
+          "yaw": -0.5021702973949935,
+          "pitch": 0.19150144995911234,
+          "rotation": 0,
+          "target": "0-hodnik-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-dnevni-borovak-2",
+      "name": "Dnevni borovak 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.75593740541985,
+        "pitch": 0.1610724793605698,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5798051248613678,
+          "pitch": 0.03618913657673417,
+          "rotation": 0,
+          "target": "10-spavaa-soba-3"
+        },
+        {
+          "yaw": -0.6791621263842984,
+          "pitch": 0.5004408772348796,
+          "rotation": 0,
+          "target": "12-hodnik-4"
+        },
+        {
+          "yaw": -0.6860677585158079,
+          "pitch": 0.05120448510233366,
+          "rotation": 0,
+          "target": "8-hodnik-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-spavaa-soba-3",
+      "name": "Spavaća soba 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.3663360608213253,
+        "pitch": -0.019446732326841243,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0299302859498187,
+          "pitch": 0.08429800413016864,
+          "rotation": 0,
+          "target": "9-dnevni-borovak-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-kupatilo-2",
+      "name": "Kupatilo 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.8878707192919641,
+        "pitch": 0.24346412898459313,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2232018693752096,
+          "pitch": 0.22371026093265556,
+          "rotation": 0,
+          "target": "12-hodnik-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-hodnik-4",
+      "name": "Hodnik 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.871789469508955,
+        "pitch": 0.16357759911121406,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6829547231850661,
+          "pitch": 0.15487150058773125,
+          "rotation": 0,
+          "target": "11-kupatilo-2"
+        },
+        {
+          "yaw": 2.8014062956447034,
+          "pitch": 0.04956183298065042,
+          "rotation": 0,
+          "target": "10-spavaa-soba-3"
+        },
+        {
+          "yaw": -2.46844894274329,
+          "pitch": 0.23159151381932652,
+          "rotation": 0,
+          "target": "9-dnevni-borovak-2"
+        },
+        {
+          "yaw": 0.6808255607916784,
+          "pitch": 0.0733382860283811,
+          "rotation": 0,
+          "target": "8-hodnik-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-terasa",
+      "name": "Terasa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.7506636389869223,
+        "pitch": 0.12193744091149128,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.922047368797493,
+          "pitch": 0.15680724684319003,
+          "rotation": 0,
+          "target": "5-dnevni-borovak"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Bulevar Vojvode Mišića",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
